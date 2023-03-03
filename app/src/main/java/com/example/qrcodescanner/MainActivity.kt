dc1f0fbf501953ee64c6fb428523e4bd531c6f30
Pcheloslav <slav.malink@gmail.com>
2023-03-04 01:52:30 +0300

    added camera, russian language support & saving images

--- app/src/main/java/com/example/qrcodescanner/MainActivity.kt
+++ app/src/main/java/com/example/qrcodescanner/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.qrcodescanner
 
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.ContentValues
 import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Color
+import android.media.MediaScannerConnection
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -26,32 +29,47 @@ import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
 import com.google.zxing.BarcodeFormat
+import com.google.zxing.EncodeHintType
 import com.google.zxing.MultiFormatWriter
 import com.google.zxing.WriterException
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
 import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
-    var imageuri: Uri? = null
     var imguri: Uri? = null
     private var barcodeScannerOptions: BarcodeScannerOptions? = null
     private var barcodeScanner:BarcodeScanner? = null
     private lateinit var storagePermissions :Array<String>
+    private lateinit var cameraPermissions :Array<String>
     var fileOut:FileOutputStream? = null
     private val STORAGE_REQUEST_CODE = 100
+    private val CAMERA_REQUEST_CODE = 101
     @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        imguri = null
         storagePermissions = arrayOf(android.Manifest.permission.MANAGE_EXTERNAL_STORAGE)
+        cameraPermissions = arrayOf(android.Manifest.permission.CAMERA)
 
 
         barcodeScannerOptions = BarcodeScannerOptions.Builder().setBarcodeFormats(Barcode.FORMAT_ALL_FORMATS).build()
         barcodeScanner = BarcodeScanning.getClient(barcodeScannerOptions!!)
+
+        cameraBtn.setOnClickListener {
+            if (checkCameraPermissions() && checkPermission())
+            {
+                pickImageCamera()
+            }
+            else
+            {
+                requestCameraPermissions()
+            }
+        }
 
         generate.setOnClickListener {
             val text = resultText.text.toString().trim()
@@ -62,8 +80,19 @@ class MainActivity : AppCompatActivity() {
             }else{
 
                 val bitmap = generateQRCode(text.trim())
+                saveImageToExternalStorage(this, bitmap)
                 img.setImageBitmap(bitmap)
                 //imguri = getimguri(this, bitmap)
+            }
+        }
+        browser.setOnClickListener {
+            val text = resultText.text.toString().trim()
+            if (!text.isEmpty()) {
+                openWebPage(text)
+            }
+            else
+            {
+                Toast.makeText(this,"Empty",Toast.LENGTH_SHORT).show()
             }
         }
 
@@ -104,6 +133,17 @@ class MainActivity : AppCompatActivity() {
         )
         return Uri.parse(path)
     }
+    @SuppressLint("QueryPermissionsNeeded")
+    fun openWebPage(url: String) {
+        var webpage = Uri.parse(url)
+        if (!url.startsWith("http://") && !url.startsWith("https://")) {
+            webpage = Uri.parse("http://$url")
+        }
+        val intent = Intent(Intent.ACTION_VIEW, webpage)
+            //if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        //}
+    }
 
 
     private fun detectResultsFromImage() {
@@ -133,7 +173,7 @@ class MainActivity : AppCompatActivity() {
             for (barcode in barcodes) {
                 val bound = barcode.boundingBox
                 val corners = barcode.cornerPoints
-                val rawvalue = barcode.rawValue
+                val rawvalue = barcode.rawBytes
                 val valueType = barcode.valueType
                 when (valueType) {
                     Barcode.TYPE_URL -> {
@@ -145,7 +185,8 @@ class MainActivity : AppCompatActivity() {
                     else -> {
                         //showToast("бляяяяяяяяяяя")
                         //resultText.text.clear()
-                        resultText.setText(rawvalue.toString())
+                        resultText.setText(String(rawvalue!!, Charsets.UTF_8))
+
                     }
                 }
 
@@ -160,6 +201,31 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = "image/*"
         galleryActivityResultLauncher.launch(intent)
+    }
+    private fun pickImageCamera()
+    {
+        val contentValues = ContentValues()
+        contentValues.put(MediaStore.Images.Media.DESCRIPTION, "Image_" + ".jpg")
+        contentValues.put(MediaStore.Images.Media.TITLE, "image/jpeg")
+        imguri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imguri)
+        cameraActivityResultLauncher.launch(intent)
+    }
+    private val cameraActivityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+    {result->
+        if (result.resultCode == Activity.RESULT_OK)
+        {
+            val data = result.data
+            Log.d("MAIN_TAG", "img $imguri")
+            //imguri = data?.data
+            img.setImageURI(imguri)
+        }
+        else
+        {
+            showToast("aaaaaaaaa")
+        }
+
     }
     private val galleryActivityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult())
     {result->
@@ -181,16 +247,17 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, message,Toast.LENGTH_SHORT).show()
     }
 
-    @RequiresApi(Build.VERSION_CODES.R)
-    private fun checkStoragePermissions():Boolean
+
+    private fun checkCameraPermissions():Boolean
     {
-        val res = ContextCompat.checkSelfPermission(this@MainActivity, android.Manifest.permission.MANAGE_EXTERNAL_STORAGE) ==
+        val res = ContextCompat.checkSelfPermission(this@MainActivity, android.Manifest.permission.CAMERA) ==
                 PackageManager.PERMISSION_GRANTED
-        return res
+        val resstorrage = checkPermission()
+        return res && resstorrage
     }
-    private fun requestStoragePermissions()
+    private fun requestCameraPermissions()
     {
-        ActivityCompat.requestPermissions(this@MainActivity, storagePermissions, STORAGE_REQUEST_CODE)
+        ActivityCompat.requestPermissions(this@MainActivity, cameraPermissions, CAMERA_REQUEST_CODE)
     }
     private val storageActivityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
         Log.d(TAG, "storageActivityResultLauncher: ")
@@ -200,7 +267,6 @@ class MainActivity : AppCompatActivity() {
             if (Environment.isExternalStorageManager()){
                 //Manage External Storage Permission is granted
                 Log.d(TAG, "storageActivityResultLauncher: Manage External Storage Permission is granted")
-                createFolder()
             }
             else{
                 //Manage External Storage Permission is denied....
@@ -251,36 +317,50 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun createFolder(){
-        //folder name
-        val folderName = "folder"
 
-        //create folder using name we just input
-        val file = File("${Environment.getExternalStorageDirectory()}/$folderName")
-        //create folder
-        val folderCreated = file.mkdir()
-
-        //show if folder created or not
-        if (folderCreated) {
-            showToast("Folder Created: ${file.absolutePath}")
-        } else {
-            showToast("Folder not created....")
+    private fun saveImageToExternalStorage(context: Context, finalBitmap: Bitmap) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val resolver = context.contentResolver
+            val contentValues = ContentValues()
+            contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, "Image_" + ".jpg")
+            contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            contentValues.put(
+                MediaStore.MediaColumns.RELATIVE_PATH,
+                Environment.DIRECTORY_PICTURES + File.separator + "TestFolder"
+            )
+            val imageUri: Uri? =
+                resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+            try {
+                val outputStream = resolver.openOutputStream(Objects.requireNonNull(imageUri!!))
+                finalBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+                Objects.requireNonNull(outputStream)
+                Toast.makeText(context, "Image Saved", Toast.LENGTH_SHORT).show()
+            } catch (e: java.lang.Exception) {
+                Toast.makeText(context, "Image Not Not  Saved: \n $e", Toast.LENGTH_SHORT).show()
+                e.printStackTrace()
+            }
         }
+
+
+
     }
 
     private fun generateQRCode(text: String): Bitmap {
         val width = 150
         val height = 150
+        val hints: Hashtable<EncodeHintType, String> = Hashtable<EncodeHintType, String>()
+        hints.put(EncodeHintType.CHARACTER_SET, "utf-8")
         val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         val codeWriter = MultiFormatWriter()
         try {
             val bitMatrix =
-                codeWriter.encode(text, BarcodeFormat.QR_CODE, width, height)
+                codeWriter.encode(text, BarcodeFormat.QR_CODE, width, height, hints)
             for (x in 0 until width) {
                 for (y in 0 until height) {
                     val color = if (bitMatrix[x, y]) Color.BLACK else Color.WHITE
                     bitmap.setPixel(x, y, color)
                 }
+
             }
         } catch (e: WriterException) {
 
@@ -310,6 +390,17 @@ override fun onRequestPermissionsResult(
                     }
                 }
             }
+            CAMERA_REQUEST_CODE-> {
+                if (grantResults.isNotEmpty()) {
+                    val storageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    if (storageAccepted) {
+                        pickImageCamera()
+                    } else {
+                        showToast("Camera permissions rrequired")
+                    }
+                }
+            }
+
 
         }
     }
